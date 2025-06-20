62609e178962652ef1d754586f954e44fe0531da
Khang <akhang147@gmail.com>
2025-06-20 16:32:29 +0700

    Refactor class CSS and write new logic open modal

--- main.js
+++ main.js
@@ -1,22 +1,30 @@
 // console.log(alert('Xin chào'));
 var modal = document.querySelector(".modal");
+var buyBTNs = document.querySelectorAll(".btn-buy");
 var closeButton = document.querySelector(".btn-close");
 var closeModal = document.getElementById("close-modal");
+const modalContainer = document.querySelector(".container-modal");
 
-var buyBtn = document.querySelectorAll(".btn-buy");
+function openModal() {
+  modal.classList.add("open");
+}
+
+for (const buyBTN of buyBTNs) {
+  buyBTN.addEventListener("click", openModal);
+}
 
 closeButton.addEventListener("click", function (e) {
-  // closeButton.style.display ='none';
-  modal.style.display = "none";
+  modal.classList.remove("open");
 });
 
 closeModal.addEventListener("click", function (e) {
-console.log('clear modal');
-  modal.style.display = "none";
-
+  modal.classList.remove("open");
 });
 
-buyBtn.addEventListener("click", function (e) {
-//    modal.style.display = "none";
-console.log('clear modal');
+modal.addEventListener("click", function () {
+  modal.classList.remove("open");
 });
+
+modalContainer.addEventListener("click", function(e){
+  e.stopPropagation();
+})
